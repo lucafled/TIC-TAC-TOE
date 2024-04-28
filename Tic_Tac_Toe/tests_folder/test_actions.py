9063d1016e873463ffe241a1b00e9549413ebe49
lucafled <165194692+lucafled@users.noreply.github.com>
2024-04-28 18:35:49 -0400

    Update test_actions.py

---
+++ Tic_Tac_Toe/tests_folder/test_actions.py
@@ -0,0 +1,18 @@
+checkhi = 'hi'
+check = 'ok'
+
+
+for instructions in check:
+    if instructions == "hi":
+        continue  # This skips the current iteration and moves to the next one
+    print (input('Hi welcome to tic tac toe (write "hihi" to continue):'))
+    break
+
+for instructions in check:
+    if instructions == "ok":
+        continue  # This skips the current iteration and moves to the next one
+    print (input('In order to win the game complete 3 spaces in diagonal or a straight line by inputing a position with the corresponding number (write "ok" to continue):'))
+    break
+
+#####################################################################################
+
