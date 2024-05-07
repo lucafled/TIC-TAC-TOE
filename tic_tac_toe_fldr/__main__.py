8881eb7dedf86d63cdc036ece99714468f28038f
M1lo-12 <165198883+M1lo-12@users.noreply.github.com>
2024-05-07 08:54:50 -0400

    Update __main__.py

--- tic_tac_toe_fldr/__main__.py
+++ tic_tac_toe_fldr/__main__.py
@@ -1,15 +1,13 @@
-
 # reference links  ->
 # https://www.youtube.com/watch?v=dK6gJw4-NCo
 # author: Code Coach
+
 
 # https://www.youtube.com/watch?v=M3G1ZgOMFxo
 # author: Shaun Halverson
 
 
-from board import squares_board, gameboard
-from actions import insertLetter, playerMove, compMove 
-from actions import checkForWin, playerMove, compMove, checkForDraw    #all the checks
+
 
 
 
@@ -17,36 +15,31 @@ checkhi = 'hi'
 check = 'ok'
 
 
-for instructions in check:
-    if instructions == "hi":
-        continue  # This skips the current iteration and moves to the next one
-    print (input('Hi welcome to tic tac toe (write "hi" to continue):'))
-    break
 
 for instructions in check:
-    if instructions == "ok":
-        continue  # This skips the current iteration and moves to the next one
-    print (input('In order to win the game complete 3 spaces in diagonal or a straight line by inputing a position with the corresponding number (write "ok" to continue):'))
-    break
-
-plrO = 'O'
-plrX = 'X'
+   if instructions == "hi":
+       continue  # This skips the current iteration and moves to the next one
+   print (input('Hi welcome to tic tac toe (write "hi" to continue):'))
+   break
 
 
+for instructions in check:
+   if instructions == "ok":
+       continue  # This skips the current iteration and moves to the next one
+   print (input('In order to win the game complete 3 spaces in diagonal or a straight line by inputing a position with the corresponding number (write "ok" to continue):'))
+   break
 
-dancex = ["┏( ͡❛ ͜ʖ ͡❛)┛ X wins! ┏( ͡❛ ͜ʖ ͡❛)┛", "┛( ͡❛ ͜ʖ ͡❛)┏ X wins! ┛( ͡❛ ͜ʖ ͡❛)┏", ]
-danceo = ["┏( ͡❛ ͜ʖ ͡❛)┛ O wins! ┏( ͡❛ ͜ʖ ͡❛)┛", "┛( ͡❛ ͜ʖ ͡❛)┏ O wins! ┛( ͡❛ ͜ʖ ͡❛)┏", ]
-repeat_times = 3
+
+from board import squares_board, gameboard
+from actions import insertLetter, playerMove, compMove
+from actions import checkForWin, playerMove, compMove, checkForDraw    #all the checks
+
+
 
 
 while not checkForWin():
-    compMove()
-    playerMove()
-
-
-
-
-
-
-
-
+   compMove()
+   playerMove()
+else:
+   if checkForWin == True:
+    instructions
