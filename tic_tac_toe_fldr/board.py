9abea10271f2bbabf0158ae02c7d4e2a4db05933
lucafled <165194692+lucafled@users.noreply.github.com>
2024-05-14 01:44:30 -0400

    Update board.py

--- tic_tac_toe_fldr/board.py
+++ tic_tac_toe_fldr/board.py
@@ -3,8 +3,8 @@ squares_board= {1: ' ', 2: ' ', 3: ' ',
                 4: ' ', 5: ' ', 6: ' ',
                 7: ' ', 8: ' ', 9: ' '}
 
-player = 'O'
-bot = 'X'
+plrO = 'O'
+plrX = 'X'
 
 
 #printBoard
